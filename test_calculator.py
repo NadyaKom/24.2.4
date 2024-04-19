0cb754bcd6ab2dea97e055537427e073606f45cc
NadyaKom <157776401+NadyaKom@users.noreply.github.com>
2024-04-19 10:43:04 +0700

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -16,11 +16,11 @@ class TestCalc:
     def test_multiply(self):
         assert self.calc.multiply(self, 2, 2) == 4
 
-    def test_divide(self):
-        assert self.calc.division(self, 6, 3) == 2
+    def test_division(self):
+        assert self.calc.division(self, 4, 2) == 2
 
     def test_subtraction(self):
-        assert self.calc.subtraction(self, 10, 7) == 3
+        assert self.calc.subtraction(self, 4, 2) == 2
     def teardown(self):
         print('Выполнение метода Teardown')
 
